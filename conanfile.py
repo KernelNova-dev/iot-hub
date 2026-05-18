a83728bae37db97342d8cfcffb9578a4a913ec03
KernelNova <daryna.vasylchenko@gmail.com>
2026-05-18 16:21:36 +0000

    Reworked dependencies; updated genrators section

--- conanfile.py
+++ conanfile.py
@@ -1,33 +1,37 @@
+# SPDX-FileCopyrightText: 2026 Daryna Vasylchenko (KernelNova) <daryna.vasylchenko@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from conan import ConanFile
+from conan.tools.gnu import PkgConfigDeps
 from conan.tools.meson import Meson, MesonToolchain
 from conan.tools.env import VirtualBuildEnv
 
-class IoTHubConan(ConanFile):
-    name = "iot-hub"
+class WireGateConan(ConanFile):
+    name = "wiregate"
     version = "1.0"
-
     settings = "os", "compiler", "build_type", "arch"
-
-    generators = "PkgConfigDeps"
 
     def requirements(self):
         self.requires("nlohmann_json/3.12.0")  # JSON-протокол IPC / конфігурація
         self.requires("spdlog/[>=1.14 <2]")    # логування
-        self.requires("fmt/[>=12.1]")          # форматування рядків, часто використовується зі spdlog
         self.requires("cli11/[>=2.4 <3]")      # аргументи CLI-утиліти
-        #self.requires("yaml-cpp/[>=0.8 <1]")   # конфігураційні файли YAML
-        self.requires("asio/[>=1.30 <2]")      # async I/O
+        self.requires("asio/1.29.0")           # async I/O shared by IPC and Crow
+        self.requires("crowcpp-crow/1.3.2")    # embedded web server for dashboard
 
-    def tool_requirements(self):
+    def build_requirements(self):
         self.tool_requires("meson/[>=1.4 <2]")
         self.tool_requires("ninja/[>=1.11 <2]")
 
     def generate(self):
+        # Meson
         tc = MesonToolchain(self)
         tc.generate()
-
+        # Venv
         env = VirtualBuildEnv(self)
         env.generate()
+        # PkgConfig
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
     def build(self):
         meson = Meson(self)
